52da7fabbeccadf3a4c32ef98190a1ece65f18a3
Kaue Henrique <115361893+KaueS771@users.noreply.github.com>
2023-06-12 19:27:24 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+function logar(){
+    var email = document.getElementById("email");
+    var senha = document.getElementById("senha");
+
+    console.log(email.value+senha.value);
+    
+    if(email.value == "admin@admin.com" && senha.value == "admin"){
+        localStorage.setItem("acesso", true);
+        alert("Usuario autenticado!")
+
+       window.location.href = "home.html";
+    }else{
+        alert("Usuario ou senha invalido!");
+    }
+}
